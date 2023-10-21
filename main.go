e4c35fa054bacf3d8f26dbd32f4429e5ba29f955
Ramazan <idkfa3215@gmail.com>
2023-10-21 15:40:01 +0300

    Comments added, extra lines

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 func main() {
 	println("Hello world")
 	a := 5
-	b := &a
+	b := &a // Указывает на память, где находится переменная a
 	println(b)
-	println(a + *b)
+	println(a + *b) // *b = a
+	println(a)
+	println(a + a)
 }
